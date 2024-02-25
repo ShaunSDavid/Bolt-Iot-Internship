f372c1f0769a67d3f7ebada3726affc559eb71cb
Shaun Sylvain David <shaunsylvaindavid@gmail.com>
2024-02-25 19:03:04 +0530

    Added Comments

--- Photo gallery/script.js
+++ Photo gallery/script.js
@@ -1,3 +1,4 @@
+// Array containing image paths
 var images = [
   "images/pictures/1.jpg",
   "images/pictures/2.jpg",
@@ -6,6 +7,7 @@ var images = [
   "images/pictures/5.jpg",
   "images/pictures/6.jpg",
 ];
+// Array containing captions for images
 var captions = [
   "A PLAINTFUL STORY FROM A SISTERING VALE",
   "BROTHERHOOD IN SISTERING VALE",
@@ -14,26 +16,35 @@ var captions = [
   "WHEREON THE THOUGHT MIGHT THINK SOMETIME IT SAW",
   "FROM OFF A HILL WHOSE CONCAVE WOMB REWORDED",
 ];
+
+// Event listener to show loading icon when image starts loading
 document.getElementById("fullImage").addEventListener("loadstart", function () {
-  document.querySelector(".loading-icon").style.display = "block"; // Show loading icon when the image starts loading
+  document.querySelector(".loading-icon").style.display = "block";
 });
 
+// Event listener to hide loading icon when image finishes loading
 document.getElementById("fullImage").addEventListener("load", function () {
-  document.querySelector(".loading-icon").style.display = "none"; // Hide loading icon when the image finishes loading
+  document.querySelector(".loading-icon").style.display = "none";
 });
 
+// Event listener to hide loading icon if image fails to load
 document.getElementById("fullImage").addEventListener("error", function () {
-  document.querySelector(".loading-icon").style.display = "none"; // Hide loading icon if image fails to load
+  document.querySelector(".loading-icon").style.display = "none";
 });
 
+// Function to remove thumbnail section
 function removeThumbnail() {
   var thumbnail = document.getElementById("thumbnail-container");
   thumbnail.style.display = "none";
 }
+
+// Function to display thumbnail section
 function displayThumbnail() {
   var thumbnail = document.getElementById("thumbnail-container");
   thumbnail.style.display = "";
 }
+
+// Function to dynamically generate thumbnails
 function generateThumbnails() {
   var thumbnailContainer = document.querySelector(".thumbnail-images");
   thumbnailContainer.innerHTML = ""; // Clear existing thumbnails
@@ -50,8 +61,10 @@ function generateThumbnails() {
   }
 }
 
+// Call generateThumbnails function when the window loads
 window.onload = generateThumbnails();
 
+// Function to highlight selected thumbnail
 function highlight(imageSrc) {
   var thumbnailContainer = document.querySelector(".thumbnail-images");
   var thumbNails = thumbnailContainer.getElementsByTagName("img");
@@ -71,16 +84,10 @@ function highlight(imageSrc) {
   }
 }
 
+// Variable to keep track of current image index
 var currentindex = 0;
-function showImage(imageSrc) {
-  var fullImage = document.getElementById("fullImage");
-  var captionText = document.getElementById("caption");
-  fullImage.src = imageSrc;
-  fullImage.style.display = "block";
-  currentindex = images.indexOf(imageSrc);
-  captionText.innerHTML = captions[currentindex];
-  highlight(imageSrc);
-}
+
+// Function to display previous image
 function previmg() {
   currentindex = (currentindex - 1 + images.length) % images.length;
   var fullImage = document.getElementById("fullImage");
@@ -90,6 +97,7 @@ function previmg() {
   highlight(fullImage.src);
 }
 
+// Function to display next image
 function nextimg() {
   currentindex = (currentindex + 1) % images.length;
   var fullImage = document.getElementById("fullImage");
